b0cea80c01b835bdee7dc272b37d43c3b0953a98
hermsale <hermsale@gmail.com>
2026-06-23 00:35:28 -0300

    feat(web) se creo y trabajo en el componente OrderDetailsCard.  Versión de componente 0.0 -> 1.0. Se trabajo en el componente OrderDetailsCard.  Próximos pasos: agregar card de Archivo, Método de Pago, y Punto de Entrega. Y el botón para cancelar el Pedido Actual

---
+++ desarrollo/web/src/features/pedidos/components/pedido_actual/OrderJobCard.tsx
@@ -0,0 +1,37 @@
+import { Order } from "../../types/order";
+
+
+interface OrderJobCardProps
+{
+  price: Order["price"];
+  createdAt:Order["createdAt"]
+}
+
+export function OrderJobCard({
+  price,
+  createdAt
+}: OrderJobCardProps){
+    return(
+<section className="order-card">
+        <h2>Información del pedido</h2>
+            <p> Fecha de creación: </p>
+
+            <strong>
+              {new Date(
+                createdAt
+              ).toLocaleString("es-AR", { dateStyle: "short", timeStyle: "short"})}
+            </strong>
+
+            <p>
+              Precio: 
+            <strong>
+               $ 
+              {price.toLocaleString(
+                "es-AR"
+              )}
+            </strong>
+            </p>
+
+</section>
+    )
+}
--- desarrollo/web/src/features/pedidos/pages/PedidoActualPage.tsx
+++ desarrollo/web/src/features/pedidos/pages/PedidoActualPage.tsx
@@ -7,13 +7,20 @@ import { ClienteLayout } from "@/layouts/cliente/ClienteLayout";
 import { getLastOrder } from "../services/order-storage";
 import { formatFileSize } from "@/features/utils/formatFileSize";
 
+
+import { useCreateOrder } from "../context/CreateOrderContext";
+
 import { CurrentOrderHeader } from "../components/pedido_actual/CurrentOrderHeader";
 import { OrderStatusTimeline } from "../components/pedido_actual/OrderStatusTimeline";
 
 import { Order } from "../types/order";
+import { OrderDetailsCard } from "../components/pedido_actual/OrderDetailsCard";
+import { OrderJobCard } from "../components/pedido_actual/OrderJobCard";
 
 export function PedidoActualPage() {
   const [order, setOrder] = useState<Order | null>(null);
+  
+   const { form, setForm } =  useCreateOrder();
 
   const [loading, setLoading] = useState(true);
 
@@ -52,6 +59,7 @@ export function PedidoActualPage() {
 
   return (
     <ClienteLayout>
+      
       <div className="dashboard-main">
 
         <CurrentOrderHeader />
@@ -60,90 +68,41 @@ export function PedidoActualPage() {
           status={order.status}
         />
 
-        {/* <div className="order-grid">
+         <div className="order-main-grid">
 
-          <section className="panel-card">
-            <h2>
-              Información del pedido
-            </h2>
+          
+          <OrderDetailsCard
+            form={order.form}
+          />
+            {/* <section className="order-card">
+              <h2>Detalles del trabajo</h2>
+                
+            </section> */}
 
-            <p>
-              Fecha de creación:
-            </p>
+            <section className="order-card">
+              <h2>Archivo</h2>
+            </section>
 
-            <strong>
-              {new Date(
-                order.createdAt
-              ).toLocaleString("es-AR")}
-            </strong>
+            {/* <section className="order-card order-highlight-card">
+              <h2>Información del pedido</h2>
+            </section> */}
+            <OrderJobCard 
+              price={order.price}
+              createdAt={order.createdAt}
+            />
 
-            <p>
-              Precio estimado:
-            </p>
+          </div>
 
-            <strong>
-              $
-              {order.price.toLocaleString(
-                "es-AR"
-              )}
-            </strong>
-          </section>
+          <div className="order-secondary-grid">
+              <section className="order-card">
+                <h2>Método de pago</h2>
+              </section>
 
-          <section className="panel-card">
-            <h2>Archivo</h2>
-
-            <p>
-              {order.fileName}
-            </p>
-
-            <p>
-              {formatFileSize(
-                order.fileSize
-              )}
-            </p>
-          </section>
-
-          <section className="panel-card">
-            <h2>Detalles</h2>
-
-            <p>
-              Páginas: {order.form.pages}
-            </p>
-
-            <p>
-              Copias: {order.form.copies}
-            </p>
-
-            <p>
-              Tamaño: {order.form.paperSize}
-            </p>
-
-            <p>
-              Impresión: {order.form.printType}
-            </p>
-          </section>
-
-          <section className="panel-card">
-            <h2>
-              Entrega y pago
-            </h2>
-
-            <p>
-              Método:
-              {" "}
-              {order.form.paymentMethod}
-            </p>
-
-            <p>
-              Punto:
-              {" "}
-              {order.form.deliveryPointId}
-            </p>
-          </section>
-
-        </div> */}
-
-      </div>
+              <section className="order-card">
+                <h2>Punto de entrega</h2>
+              </section>
+            </div>
+          </div>
     </ClienteLayout>
   );
 }
